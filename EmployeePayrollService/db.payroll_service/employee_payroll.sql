3c0e2d01d7719a2547abb2432603ec5203bfec0b
vk0808 <vk4765712@gmail.com>
2021-09-06 09:54:46 +0530

    feat: create database

---
+++ EmployeePayrollService/db.payroll_service/employee_payroll.sql
@@ -0,0 +1,11 @@
+-- uc-1
+
+-- To Create a database
+CREATE DATABASE payroll_service
+
+-- To see the DB created
+EXEC sp_databases
+--SELECT * FROM sys.databases;
+
+-- To go to the database created
+USE payroll_service
