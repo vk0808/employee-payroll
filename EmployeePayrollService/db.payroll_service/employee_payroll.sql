6819692eee7a985ea140ee8b983e61f51cbf0e14
vk0808 <vk4765712@gmail.com>
2021-09-06 10:08:12 +0530

    feat: select from table

--- EmployeePayrollService/db.payroll_service/employee_payroll.sql
+++ EmployeePayrollService/db.payroll_service/employee_payroll.sql
@@ -37,3 +37,12 @@ VALUES
 ('Bill', 1000000.00, '2021-09-02'),
 ('Terisha', 2000000.00, '2021-09-02'),
 ('Charlie', 3000000.00, '2021-09-02');
+
+---------------------------------------------------------------
+
+-- uc-4
+-- To go to the database created
+USE payroll_service
+
+-- To select all records from table
+SELECT * FROM employee_payroll;
