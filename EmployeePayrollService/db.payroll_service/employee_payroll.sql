8af51f85a73b3d2c921634b7f9167001a334ff50
vk0808 <vk4765712@gmail.com>
2021-09-06 10:04:17 +0530

    feat: insert into table

--- EmployeePayrollService/db.payroll_service/employee_payroll.sql
+++ EmployeePayrollService/db.payroll_service/employee_payroll.sql
@@ -13,6 +13,9 @@ USE payroll_service
 ---------------------------------------------------------------
 
 -- uc-2
+-- To go to the database created
+USE payroll_service
+
 -- To create a table
 CREATE TABLE employee_payroll(
 	EmployeeID INT IDENTITY(1, 1) PRIMARY KEY,
@@ -20,3 +23,17 @@ CREATE TABLE employee_payroll(
 	Salary DECIMAL NOT NULL,
 	StartDate DATE  NOT NULL
 );
+
+
+---------------------------------------------------------------
+
+-- uc-3
+-- To go to the database created
+USE payroll_service
+
+-- To insert into the table created
+INSERT INTO employee_payroll(EmployeeName, Salary, StartDate) 
+VALUES
+('Bill', 1000000.00, '2021-09-02'),
+('Terisha', 2000000.00, '2021-09-02'),
+('Charlie', 3000000.00, '2021-09-02');
